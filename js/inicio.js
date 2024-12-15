03c53a635df178d76ab3691afc22e0de6645f5be
Iago Páramo <a22iagopp@iessanclemente.net>
2024-12-16 00:43:42 +0100

    "acabado fade javascript, faltan pruebas"

--- js/inicio.js
+++ js/inicio.js
@@ -1,4 +1,39 @@
 document.addEventListener("scroll", () => {
+    const container = document.querySelector('.carrusel_container_venta');
+
+    // Intersection Observer setup
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            container.classList.remove('hidden');
+            container.classList.add('visible');
+        }
+      });
+    });
+  
+    // Observe the container
+    observer.observe(container);
+  });
+
+
+document.addEventListener("scroll", () => {
+    const container = document.querySelector('.fotos_propiedades');
+
+    // Intersection Observer setup
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            container.classList.remove('hidden');
+            container.classList.add('visible');
+        }
+      });
+    });
+  
+    // Observe the container
+    observer.observe(container);
+  });
+
+document.addEventListener("scroll", () => {
     const container = document.querySelector('.carrusel_container');
 
     // Intersection Observer setup
@@ -15,8 +50,9 @@ document.addEventListener("scroll", () => {
     observer.observe(container);
   });
 
+
   document.addEventListener("scroll", () => {
-    const container = document.querySelector('.fotos_propiedades');
+    const container = document.querySelector('.fotos_propiedades_venta');
 
     // Intersection Observer setup
     const observer = new IntersectionObserver(entries => {
@@ -31,3 +67,6 @@ document.addEventListener("scroll", () => {
     // Observe the container
     observer.observe(container);
   });
+
+
+
