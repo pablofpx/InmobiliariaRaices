a7e8ab681f23bbdb107f237f8a91007f42aad06f
Iago Páramo <a22iagopp@iessanclemente.net>
2024-12-16 00:19:03 +0100

    niquelado un poco y en desarrollo el javascript de los fades de contenedores

---
+++ js/inicio.js
@@ -0,0 +1,33 @@
+document.addEventListener("scroll", () => {
+    const container = document.querySelector('.carrusel_container');
+
+    // Intersection Observer setup
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            container.classList.remove('hidden');
+            container.classList.add('visible');
+        }
+      });
+    });
+  
+    // Observe the container
+    observer.observe(container);
+  });
+
+  document.addEventListener("scroll", () => {
+    const container = document.querySelector('.fotos_propiedades');
+
+    // Intersection Observer setup
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            container.classList.remove('hidden');
+            container.classList.add('visible');
+        }
+      });
+    });
+  
+    // Observe the container
+    observer.observe(container);
+  });
